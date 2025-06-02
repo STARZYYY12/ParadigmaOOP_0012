883103cf062f91a111166861ee51aa992a1f43a3
STARZYYY12 <rizkyfaiz324@gmail.com>
2025-06-02 10:31:58 +0700

    member class remoteLampu

---
+++ enkapsulasi/enkapsulasi.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class remoteLampu {
+    private :
+        string SaklarNo[10];
+    public :
+        void setSaklarNo(int i, string value) {
+            SaklarNo[i] = value;
+        }
+        string getSaklarNo(int i) {
+            return SaklarNo[i];
+        }
+};
+
